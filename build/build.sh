541b778cc3a95575b7e2c8add8ae480469da5790
omega <398914521@qq.com>
2025-04-04 14:20:04 +0000

    移除垃圾代码

--- build/build.sh
+++ build/build.sh
@@ -8,16 +8,6 @@ cd $curr_path
 
 source $script_path/build.env
 
-WORKSPACE=$script_path/..
-PROJ_NAME=mould
-GIT_COMMITID=`git rev-parse --short HEAD`
-BIN_NAME=$PROJ_NAME-linux-x86_64-$GIT_COMMITID
-LATEST_BIN_NAME=$PROJ_NAME-linux-x86_64-latest
-IMAGE_NAME=$PROJ_NAME-image-x86_64-$GIT_COMMITID.tar.gz
-LATEST_IMAGE_NAME=$PROJ_NAME-image-x86_64-latest.tar.gz
-CONFIG_FILE=/config/config.json5
-IMAGE_PATH=$REGISTRY$IMAGE_NAMESPACE/$PROJ_NAME
-
 error_exit() {
 	msg=$1
 	echo $msg
@@ -51,30 +41,3 @@ $script_path/build-mould.sh || error_exit "Build mould failed!"
 dos2unix $script_path/build-extensions.sh
 chmod +x $script_path/build-extensions.sh
 $script_path/build-extensions.sh || error_exit "Build extensions failed!"
-
-ls $WORKSPACE/target/release/
-
-exit 0
-
-echo "Start to package image."
-IMAGE_ROOT=$script_path/image-root
-cp $WORKSPACE/target/release/$PROJ_NAME $IMAGE_ROOT/
-chmod +x $IMAGE_ROOT/lib64/*
-chmod +x $IMAGE_ROOT/extensions/*
-chmod +x $IMAGE_ROOT/$PROJ_NAME
-cd $IMAGE_ROOT/
-tar -zcvf $script_path/$IMAGE_NAME * || error_exit "Package image failed!"
-cd $script_path/
-
-echo "Start to build image."
-docker import -c "CMD [\"/$PROJ_NAME\", \"$CONFIG_FILE\"]" $script_path/$IMAGE_NAME $IMAGE_PATH:$GIT_COMMITID || error_exit "Build image failed!"
-docker tag $IMAGE_PATH:$GIT_COMMITID $IMAGE_PATH:latest || error_exit "Add image tag failed!"
-
-rm -f $script_path/$IMAGE_NAME
-
-echo "Start to upload image artifact."
-echo $REGISTRY_PASSWORD | docker login -u $REGISTRY_USER --password-stdin $REGISTRY || error_exit "Login to registry failed!"
-docker push $IMAGE_PATH:$GIT_COMMITID || error_exit "Push image failed!"
-docker push $IMAGE_PATH:latest || error_exit "Push image failed!"
-echo image path: $IMAGE_PATH:$GIT_COMMITID
-echo "Build succeed."
